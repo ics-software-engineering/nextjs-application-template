14232ead4bd412929b37056eb6a0d61275aec70c
Cam Moore <cammymoore07@gmail.com>
2026-03-01 09:09:35 -1000

    broken
    Took 12 minutes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,7 @@
-import { PrismaClient } from '@prisma/client';
-
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-// eslint-disable-next-line import/prefer-default-export, operator-linebreak
-export const prisma =
-  // eslint-disable-next-line operator-linebreak
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'], // CAM: is this the right level of logging?
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+export { prisma };
